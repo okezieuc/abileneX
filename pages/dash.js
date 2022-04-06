ce968d6c6d6abc3d83a94c9dbf925c64dfce35c4
okezieuc <c.okezie.u@gmail.com>
2022-04-06 20:41:07 +0000

    minor change

--- pages/dash.js
+++ pages/dash.js
@@ -7,7 +7,7 @@ export default function Dashboard() {
     <AppLayout>
       <div className="py-16">
         <div>
-          <div className="text-2xl text-zinc-500 mb-7">Hi John</div>
+          <div className="text-2xl text-zinc-500 mb-7 font-light">Hi John</div>
           <h1 className="text-4xl font-medium max-w-md mb-6">It’s time to build with confidence. Create a poll.</h1>
           <div className="bg-zinc-800 rounded-full text-white text-md w-max px-6 py-2">Create Poll</div>
         </div>
