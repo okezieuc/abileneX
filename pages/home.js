df9e77e182d364e63c1a4ecc3a34ed6b26080a2d
okezieuc <c.okezie.u@gmail.com>
2022-04-08 23:53:29 +0000

    improve homepage design

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,7 @@
 import { useUser, Auth } from "@supabase/supabase-auth-helpers/react";
 import { supabaseClient } from "@supabase/supabase-auth-helpers/nextjs";
 import { useEffect, useState } from "react";
+import AppLayout from "@components/dashboard/appLayout";
 
 const LoginPage = () => {
   const { user, error } = useUser();
@@ -16,16 +17,28 @@ const LoginPage = () => {
   }, [user]);
   */
   if (!user)
+    // max-w-lg mx-auto border mt-12 p-6 rounded-lg
     return (
-      <>
-        {error && <p>{error.message}</p>}
-        <Auth
-          supabaseClient={supabaseClient}
-          providers={["google"]}
-          socialLayout="horizontal"
-          socialButtonSize="xlarge"
-        />
-      </>
+      <AppLayout>
+        <div className="grid grid-cols-2 items-center">
+          <div className="min-h-screen flex flex-col pr-8">
+            <div className="flex-1"></div>
+            {error && <p>{error.message}</p>}
+            <h1 className="text-3xl font-semibold text-left">
+              <div>Join</div> <div className="text-5xl mt-4 font-bold text-sky-600">AbileneX</div>
+            
+          </h1>
+          <p className="mt-8 text-lg">AbileneX creates sharable voting links for you to get feedback on your ideas before you start building. AbileneX lets you build with confidence.</p>
+            <div className="flex-1"></div>
+          </div>
+          <Auth
+            supabaseClient={supabaseClient}
+            providers={["google"]}
+            socialLayout="horizontal"
+            socialButtonSize="xlarge"
+          />
+        </div>
+      </AppLayout>
     );
 
   return (
