c10a44027d3604029a3d29cdf9275462c2adc7b1
okezieuc <c.okezie.u@gmail.com>
2022-04-10 20:23:10 +0000

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ function MyApp({ Component, pageProps }) {
             name="Description"
             content="Get honest feedback on your ideas so you can build with confidence."
           />
+          <link rel="icon" type="image/x-icon" href="/logo.png"></link>
         </Head>
 
         <Component {...pageProps} />
