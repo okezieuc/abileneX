af25389c81a4ed9f9584d8b837135986fb282815
okezieuc <c.okezie.u@gmail.com>
2022-04-13 08:59:33 +0000

    fetch data on poll status page with GraphQL

--- pages/dash/track/[poll_id].js
+++ pages/dash/track/[poll_id].js
@@ -8,6 +8,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
+import { supabaseGraphQLClient } from "utils/supabaseGraphQLClient";
 import StatusPageImage1 from "../../../public/girl-stretching.png";
 
 // Refactor the code in this page
@@ -15,52 +16,81 @@ import StatusPageImage1 from "../../../public/girl-stretching.png";
 // Consider fetching initial poll data on server side
 
 export default function TrackPollPage() {
-  const { user, error } = useUser();
+  const { user, accessToken, error } = useUser();
   const router = useRouter();
   const [pollData, setPollData] = useState(null);
   const [pollVoteData, setPollVoteData] = useState(null);
   const [pollVoteRatings, setPollVoteRatings] = useState(null);
   const [pollVoteComments, setPollVoteComments] = useState([]);
   const [stoppingPoll, setStoppingPoll] = useState(false);
-  const [numberOfVotesReceived, setNumberOfVotesReceived] = useState(null)
+  const [numberOfVotesReceived, setNumberOfVotesReceived] = useState(null);
 
   async function checkPollAcceptingVotes(loadingAfterStopping = false) {
     // check if the poll is still accepting responses
-    const { data } = await supabaseClient
-      .from("polls")
-      .select("poll_id, title, accepting_votes, number_of_votes_received")
-      .eq("poll_id", router.query.poll_id);
+    const data = await supabaseGraphQLClient(
+      `query LoadPollData($id: UUID!) {
+        pollsCollection(filter: {pollId: {eq: $id } }) {
+          edges {
+            node {
+              title
+              pollId
+              acceptingVotes
+              numberOfVotesReceived
+            }
+          }
+        }
+      }`,
+      {
+        authorizationKey: accessToken,
+        variables: {
+          id: router.query.poll_id,
+        },
+      }
+    );
 
-    if (data.length != 1) {
+    if (data.pollsCollection.edges.length != 1) {
       router.push("/"); // return to the homepage if we receive bad data
     }
 
-    // setPollData(data[0]); // update poll data
-
-    if (data[0].accepting_votes == true) {
-      setTimeout(() => setPollData(data[0]), 1500);
+    if (data.pollsCollection.edges[0].node.acceptingVotes == true) {
+      setTimeout(() => setPollData(data.pollsCollection.edges[0]), 1500);
       return;
     } else {
-      // continue fetching otherwise
-      const { data: voteData } = await supabaseClient
-        .from("poll_votes")
-        .select("vote_id, idea_rating, idea_comment")
-        .eq("poll_id", router.query.poll_id);
+      const voteData = await supabaseGraphQLClient(
+        `query LoadVoteData($id: UUID!) {
+          pollVotesCollection(filter: {pollId: {eq: $id } }) {
+            edges {
+              node {
+                pollId
+                voteId
+                ideaRating
+                ideaComment
+              }
+            }
+          }
+        }`,
+        {
+          authorizationKey: accessToken,
+          variables: {
+            id: router.query.poll_id,
+          },
+        }
+      );
 
       let temporaryPollVoteRatings = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
       let temportaryPollVoteComments = [];
 
-      voteData.forEach((vote) => {
-        temporaryPollVoteRatings[vote.idea_rating] =
-          temporaryPollVoteRatings[vote.idea_rating] + 1;
-        if (vote.idea_comment !== null) {
-          temportaryPollVoteComments.push(vote.idea_comment);
+      voteData.pollVotesCollection.edges.forEach((vote) => {
+        temporaryPollVoteRatings[vote.node.ideaRating] =
+          temporaryPollVoteRatings[vote.node.ideaRating] + 1;
+        if (vote.node.ideaComment !== null) {
+          temportaryPollVoteComments.push(vote.node.ideaComment);
         }
       });
 
       setPollVoteRatings(temporaryPollVoteRatings);
       setPollVoteComments(temportaryPollVoteComments);
-      setTimeout(() => setPollData(data[0]), 1000);
+      setTimeout(() => setPollData(data.pollsCollection.edges[0]), 1000);
       setPollVoteData(voteData);
     }
   }
@@ -68,46 +98,7 @@ export default function TrackPollPage() {
   useEffect(() => {
     // TODO: Duplicating this function was a hotfix
     // ..... remove the duplicate of checkPollAcceptingVotes, run `npx next lint`, and fix the error
-    async function checkPollAcceptingVotes(loadingAfterStopping = false) {
-      // check if the poll is still accepting responses
-      const { data } = await supabaseClient
-        .from("polls")
-        .select("poll_id, title, accepting_votes, number_of_votes_received")
-        .eq("poll_id", router.query.poll_id);
-
-      if (data.length != 1) {
-        router.push("/"); // return to the homepage if we receive bad data
-      }
-
-      // setPollData(data[0]); // update poll data
-
-      if (data[0].accepting_votes == true) {
-        setTimeout(() => setPollData(data[0]), 1500);
-        return;
-      } else {
-        // continue fetching otherwise
-        const { data: voteData } = await supabaseClient
-          .from("poll_votes")
-          .select("vote_id, idea_rating, idea_comment")
-          .eq("poll_id", router.query.poll_id);
-
-        let temporaryPollVoteRatings = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
-        let temportaryPollVoteComments = [];
-
-        voteData.forEach((vote) => {
-          temporaryPollVoteRatings[vote.idea_rating] =
-            temporaryPollVoteRatings[vote.idea_rating] + 1;
-          if (vote.idea_comment !== null) {
-            temportaryPollVoteComments.push(vote.idea_comment);
-          }
-        });
-
-        setPollVoteRatings(temporaryPollVoteRatings);
-        setPollVoteComments(temportaryPollVoteComments);
-        setTimeout(() => setPollData(data[0]), 1000);
-        setPollVoteData(voteData);
-      }
-    }
+    // TODO: Duplicate checkPollAcceptingVotes here
 
     if (user) checkPollAcceptingVotes();
   }, [user, router]);
@@ -119,12 +110,12 @@ export default function TrackPollPage() {
         .from(`polls:poll_id=eq.${router.query.poll_id}`)
         .on("UPDATE", (payload) => {
           console.log("Change received!", payload);
-          setNumberOfVotesReceived(payload.new.number_of_votes_received)
+          setNumberOfVotesReceived(payload.new.number_of_votes_received);
         })
         .subscribe();
     }
 
-    setupVoteCountSubscription()
+    setupVoteCountSubscription();
   }, [router.query.poll_id]);
 
   async function stopPollAcceptingVotes() {
@@ -143,21 +134,24 @@ export default function TrackPollPage() {
           {pollData ? (
             <>
               <Head>
-                <title>Poll Status: {pollData.title}</title>
+                <title>Poll Status: {pollData.node.title}</title>
               </Head>
               <div className="text-center">
                 <div className="text-2xl text-zinc-500 mb-7 font-light">
-                  {numberOfVotesReceived ? numberOfVotesReceived : pollData.number_of_votes_received } Response(s) Received
+                  {numberOfVotesReceived
+                    ? numberOfVotesReceived
+                    : pollData.node.numberOfVotesReceived}{" "}
+                  Response(s) Received
                 </div>
                 <h1 className="text-4xl font-medium max-w-md mb-6 mx-auto">
-                  {pollData.title}
+                  {pollData.node.title}
                 </h1>
               </div>
-              {pollData.accepting_votes ? (
+              {pollData.node.acceptingVotes ? (
                 <>
                   <div className="flex-1">
                     <div className="mx-auto w-min">
-                      <LinkCopyComponent id={pollData.poll_id} />
+                      <LinkCopyComponent id={pollData.node.pollId} />
                     </div>
                     <div className="w-56 mx-auto">
                       <Image src={StatusPageImage1} alt="" placeholder="blur" />
