c0f9ce21220bd1f1ca67be8b549e7f19f9fcfae4
okezieuc <c.okezie.u@gmail.com>
2022-04-13 08:26:50 +0000

    update button text to "Your vote has been sent" when the vote has been sent

--- pages/vote/[poll_id].js
+++ pages/vote/[poll_id].js
@@ -9,14 +9,17 @@ import { supabaseServiceClient } from "utils/supabaseServiceClient";
 export default function PollVotePage({ pollData }) {
   const [ideaVoteRating, setIdeaVoteRating] = useState(null);
   const [ideaText, setIdeaText] = useState(null);
-  const [sendingVote, setSendingVote] = useState(false);
+  const [sendingVote, setSendingVote] = useState(0);
 
   async function sendPollVote() {
     try {
-      setSendingVote(true);
+      if(sendingVote == 2) {
+        return
+      }
+      setSendingVote(1);
 
       if (ideaVoteRating == null) {
-        setSendingVote(false);
+        setSendingVote(0);
         return;
       }
 
@@ -32,91 +35,98 @@ export default function PollVotePage({ pollData }) {
           returning: "minimal",
         });
 
-      setSendingVote(false);
+      setSendingVote(2);
     } catch (error) {
       console.error(error);
     }
   }
-  
+
   return (
     <AppLayout>
       <>
-      <Head>
-        <title>Vote: {pollData.title}</title>
-      </Head>
-      <div className="my-16">
-        <div className="text-center">
-          <div className="text-2xl text-zinc-500 mb-7 font-light">
-            We are asking for your honest feedback
-          </div>
-          <h1 className="text-4xl font-medium max-w-md mb-6 mx-auto">
-            {pollData.title}
-          </h1>
-        </div>
-        <div>
-          <div className="flex gap-4 w-min mx-auto items-center">
-            <div className="w-max text-zinc-700 text-lg font-semibold mr-4">
-              Nah
+        <Head>
+          <title>Vote: {pollData.title}</title>
+        </Head>
+        <div className="my-16">
+          <div className="text-center">
+            <div className="text-2xl text-zinc-500 mb-7 font-light">
+              We are asking for your honest feedback
             </div>
-            <VoteButton
-              number={1}
-              setIdeaVoteRating={(rating) => setIdeaVoteRating(rating)}
-              ideaVoteRating={ideaVoteRating}
-            />
-            <VoteButton
-              number={2}
-              setIdeaVoteRating={(rating) => setIdeaVoteRating(rating)}
-              ideaVoteRating={ideaVoteRating}
-            />
-            <VoteButton
-              number={3}
-              setIdeaVoteRating={(rating) => setIdeaVoteRating(rating)}
-              ideaVoteRating={ideaVoteRating}
-            />
-            <VoteButton
-              number={4}
-              setIdeaVoteRating={(rating) => setIdeaVoteRating(rating)}
-              ideaVoteRating={ideaVoteRating}
-            />
-            <VoteButton
-              number={5}
-              setIdeaVoteRating={(rating) => setIdeaVoteRating(rating)}
-              ideaVoteRating={ideaVoteRating}
-            />
-            <div className="w-max text-zinc-700 text-lg font-semibold ml-4">
-              Awesome idea!
+            <h1 className="text-4xl font-medium max-w-md mb-6 mx-auto">
+              {pollData.title}
+            </h1>
+          </div>
+          <div>
+            <div className="flex gap-4 w-min mx-auto items-center">
+              <div className="w-max text-zinc-700 text-lg font-semibold mr-4">
+                Nah
+              </div>
+              <VoteButton
+                number={1}
+                setIdeaVoteRating={(rating) => setIdeaVoteRating(rating)}
+                ideaVoteRating={ideaVoteRating}
+              />
+              <VoteButton
+                number={2}
+                setIdeaVoteRating={(rating) => setIdeaVoteRating(rating)}
+                ideaVoteRating={ideaVoteRating}
+              />
+              <VoteButton
+                number={3}
+                setIdeaVoteRating={(rating) => setIdeaVoteRating(rating)}
+                ideaVoteRating={ideaVoteRating}
+              />
+              <VoteButton
+                number={4}
+                setIdeaVoteRating={(rating) => setIdeaVoteRating(rating)}
+                ideaVoteRating={ideaVoteRating}
+              />
+              <VoteButton
+                number={5}
+                setIdeaVoteRating={(rating) => setIdeaVoteRating(rating)}
+                ideaVoteRating={ideaVoteRating}
+              />
+              <div className="w-max text-zinc-700 text-lg font-semibold ml-4">
+                Awesome idea!
+              </div>
+            </div>
+            <div className="max-w-md mx-auto mt-9">
+              <textarea
+                type="textarea"
+                placeholder="(optional) The creator of this poll will appreciate you sharing more detail about why you voted what you did, how to improve the idea, or alternatives you suggest?"
+                className="w-full text-lg border-zinc-500 mb-4 rounded-md h-32"
+                autoFocus={true}
+                onChange={(e) => {
+                  setIdeaText(e.target.value);
+                }}
+              />
+            </div>
+            <div className="mt-12 w-max mx-auto">
+              <button
+                className="bg-zinc-800 rounded-full text-white text-md w-max px-8 py-2 mx-auto flex items-center"
+                onClick={() => sendPollVote()}
+              >
+                {sendingVote ? (
+                  <>
+                    {sendingVote == 2 ? (
+                      "Your vote has been sent"
+                    ) : (
+                      <>
+                        <span>
+                          <SpinningIcon />
+                        </span>{" "}
+                        Sending your vote
+                      </>
+                    )}
+                  </>
+                ) : (
+                  "Vote"
+                )}
+              </button>
             </div>
           </div>
-          <div className="max-w-md mx-auto mt-9">
-            <textarea
-              type="textarea"
-              placeholder="(optional) The creator of this poll will appreciate you sharing more detail about why you voted what you did, how to improve the idea, or alternatives you suggest?"
-              className="w-full text-lg border-zinc-500 mb-4 rounded-md h-32"
-              autoFocus={true}
-              onChange={(e) => {
-                setIdeaText(e.target.value);
-              }}
-            />
-          </div>
-          <div className="mt-12 w-max mx-auto">
-            <button
-              className="bg-zinc-800 rounded-full text-white text-md w-max px-8 py-2 mx-auto flex items-center"
-              onClick={() => sendPollVote()}
-            >
-              {sendingVote ? (
-                <>
-                  <span>
-                    <SpinningIcon />
-                  </span>{" "}
-                  Sending your vote{" "}
-                </>
-              ) : (
-                "Vote"
-              )}
-            </button>
-          </div>
         </div>
-      </div></>
+      </>
     </AppLayout>
   );
 }
