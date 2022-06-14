a73c8c3cce12bf0a64a4f4680ce0de2cbdedd1cc
okezieuc <c.okezie.u@gmail.com>
2022-06-14 20:10:01 +0000

    rewrite linkCopyComponent and pollListItem in ts

--- components/dashboard/dash/pollListItem.tsx
+++ components/dashboard/dash/pollListItem.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
 import RightIcon from "../icons/rightIcon";
 
+type Props = {
+  title: String;
+  poll_id: String;
+};
+
 export default function PollListItem({
   title = "Should we build an anonymous voting app?",
   poll_id,
-}) {
+}: Props) {
   return (
     <div className="group w-full flex flex-col h-40 bg-zinc-100 rounded-xl px-4 py-4 hover:shadow-sm border border-zinc-200 hover:border-zinc-300 transition-all">
       <div className="flex-1"></div>
